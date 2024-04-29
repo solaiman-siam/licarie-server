85f7a8352d83b2723586d1fce64b862c0a9b00ab
solaiman-siam <solaimansiam93@gmail.com>
2024-04-29 14:27:45 +0600

    added delete craft item route

--- index.js
+++ index.js
@@ -23,10 +23,6 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    // Send a ping to confirm a successful connection
-
     const ceramicsAndPotteryCollection = client
       .db("ceramicsAndPotteryDB")
       .collection("products");
